038960d5ad59e50a777f6dd2ff0eaa62b4087c35
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-12-04 19:04:24 -0700

    add feedback

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Reviews from './Component/Reviews';
 import SideBar from './Component/SideBar';
 import Visitors from './Component/Visitors';
 
-
+//you might want to add a dashboard component that you bring into App and then put all of these components there, that would just break it down a bit further.
 
 export default function App() {
   return (
